fa8327707d669e0c8e1247c84fc24d088cd13568
pgaref <pgaref@macbook-air.lan>
2015-02-13 17:48:06 +0000

    Re-organising repo

--- myhandler.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-use strict;
-open(FILE, ">>", "/usr/local/nagios/myeventhandler.log") or die "$!";
-print FILE "myhandler: $ARGV[0] : $ARGV[1] : $ARGV[2]\n";
-close FILE;
-print "DONE!";
